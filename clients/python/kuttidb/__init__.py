cd5663e3e6a46ad75040d78d815e9750fe82fb2d
Abdussamed Aslan <8167253+orionasa@users.noreply.github.com>
2026-09-10 06:07:53 +0300

    chore: bump clients to 0.1.3

--- clients/python/kuttidb/__init__.py
+++ clients/python/kuttidb/__init__.py
@@ -65,7 +65,7 @@ from .client import (
 from .embed import KuttiEmbed, KuttiEmbedError
 from .local import LocalKuttiDB
 
-__version__ = "0.1.0"
+__version__ = "0.1.3"
 
 __all__ = [
     "CAP_ATOMIC",
